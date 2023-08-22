e251282e96e4808eee2b9e660e196d51218e8a91
BharadwajDevarsetty1 <109033650+BharadwajDevarsetty1@users.noreply.github.com>
2023-08-21 17:45:04 -1000

    Update streamlit2_app.py

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -1,33 +1,46 @@
 import streamlit
 import snowflake.connector
-import pandas
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
-# connect to snowflake
+import pandas as pd  # Using "as pd" for pandas alias
+
+# Set the title
+streamlit.title("Zena's Amazing Athleisure Catalog")
+
+# Connect to Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
+
+# Run a Snowflake query and fetch data
+my_cur.execute("SELECT color_or_style FROM catalog_for_website")
 my_catalog = my_cur.fetchall()
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
-# print(color_list)
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-# We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-# use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
-df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
+
+# Create a DataFrame
+df = pd.DataFrame(my_catalog, columns=["Color or Style"])
+
+# Get a list of colors/styles
+color_list = df["Color or Style"].tolist()
+
+# Selectbox for color/style
+option = streamlit.selectbox("Pick a sweatsuit color or style:", color_list)
+
+# Fetch additional product information
+my_cur.execute(
+    "SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = %s;",
+    (option,),
 )
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])
+df2 = my_cur.fetchone()
+
+# Display product information
+if df2:
+    product_caption = f"Our warm, comfortable, {option} sweatsuit!"
+    streamlit.image(df2[0], width=400, caption=product_caption)
+    streamlit.write("Price:", df2[1])
+    streamlit.write("Sizes Available:", df2[2])
+    streamlit.write(df2[3])
+else:
+    streamlit.write("No information found for the selected color/style.")
+
+# Close the cursor and connection
+my_cur.close()
+my_cnx.close()
+
 
